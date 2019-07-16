0fc3df598df8127a00fcafdf1e663a16aad22c6c
Giles Cope <gilescope@gmail.com>
2019-07-16 18:18:06 +0100

    Renaming

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
 #![feature(async_await)]
 use std::future::Future;
 
-pub trait Broadcast {
+pub trait T {
     type Future1: Future<Output = Vec<()>>;
     type Future2: Future<Output = String>;
 
-    fn broadcast1() -> Self::Future1;
-    fn broadcast2() -> Self::Future2;
+    fn call1() -> Self::Future1;
+    fn call2() -> Self::Future2;
 }
-pub async fn propose<B>() where B: Broadcast {
-    B::broadcast1().await;
-    B::broadcast2().await;
+pub async fn await_two_different_return_types<S>() where S: T {
+    S::call1().await;
+    S::call2().await;
 }
