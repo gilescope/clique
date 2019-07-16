d7a8907a448e94b2109db952fe9cd9d161261f2a
Giles Cope <gilescope@gmail.com>
2019-07-16 18:07:00 +0100

    manual reduce

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,14 @@
 #![feature(async_await)]
 use std::future::Future;
-use tokio_sync::oneshot;
-pub trait Client {
-    type Future: Future<Output = Response>;
-    fn call(&mut self) -> Self::Future;
-}
+
 pub trait Broadcast {
-    type Future: Future<Output = Vec<Response>>;
-    fn broadcast(&mut self, req: Request) -> Self::Future;
+    type Future1: Future<Output = Vec<()>>;
+    type Future2: Future<Output = String>;
+
+    fn broadcast1() -> Self::Future1;
+    fn broadcast2() -> Self::Future2;
 }
-pub struct Request {}
-pub struct Response {}
-impl Request {
-    pub fn new(res_tx: oneshot::Sender<Response>) -> Self {
-        unimplemented!()
-    }
-}
-pub struct Paxos<'a, C> {
-    client: &'a mut C,
-}
-pub struct FastPaxos<'a, B> {
-    broadcast: &'a mut B,
-}
-impl<'a,  B> FastPaxos<'a, B>
-    where
-        B: Broadcast,
-{
-    pub async fn propose(&mut self) -> () {
-        let (tx, rx) = oneshot::channel();
-        let request = Request::new(tx);
-        self.broadcast.broadcast(request).await;
-        rx.await;
-    }
+pub async fn propose<B>() where B: Broadcast {
+    B::broadcast1().await;
+    B::broadcast2().await;
 }
