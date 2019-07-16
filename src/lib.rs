9ef36d8da48ad95f3b2aa1d11ee4aa56492b11a5
Giles Cope <gilescope@gmail.com>
2019-07-16 18:27:08 +0100

    manual simplification

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,10 @@
 use std::future::Future;
 
 pub trait T {
-    type Future1: Future<Output = Vec<()>>;
-    type Future2: Future<Output = String>;
-
-    fn call1() -> Self::Future1;
-    fn call2() -> Self::Future2;
+    type Future: Future<Output = String>;
+    fn bar() -> Self::Future;
 }
-pub async fn await_two_different_return_types<S>() where S: T {
-    S::call1().await;
-    S::call2().await;
+pub async fn foo<S>() where S: T {
+    S::bar().await;
+    S::bar().await;
 }
