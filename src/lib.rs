51cfb2d1f83168b4e88c80c6bc06cff3a3a7926e
Giles Cope <gilescope@gmail.com>
2019-07-16 09:39:58 +0100

    reduce

--- src/lib.rs
+++ src/lib.rs
@@ -7,13 +7,6 @@ pub mod transport {
     pub mod proto {
         pub type ConfigId = i64;
         pub type Endpoint = String;
-        pub enum RequestKind {
-            Consensus(),
-        }
-        pub enum Consensus {
-            FastRoundPhase2bMessage(),
-        }
-        pub struct FastRoundPhase2bMessage {}
         pub struct Phase2bMessage {}
     }
     use std::future::Future;
@@ -31,23 +24,18 @@ pub mod transport {
     pub struct Request {}
     pub struct Response {}
     impl Request {
-        pub fn new(
-            res_tx: oneshot::Sender<crate::Result<Response>>,
-          //  kind: proto::RequestKind,
-        ) -> Self {
+        pub fn new(res_tx: oneshot::Sender<crate::Result<Response>>) -> Self {
             unimplemented!()
         }
     }
 }
 mod consensus {
-    use futures::{stream::FuturesUnordered, Future, FutureExt, Stream, StreamExt};
     use std::{
         collections::{HashMap, HashSet},
         sync::atomic::{AtomicBool, AtomicUsize, Ordering},
         time::{Duration, Instant},
     };
     use tokio_sync::oneshot;
-    use tokio_timer::Delay;
     mod paxos {
         pub struct Paxos<'a, C> {
             client: &'a mut C,
@@ -63,14 +51,12 @@ mod consensus {
     use paxos::Paxos;
     pub struct FastPaxos<'a, C, B> {
         broadcast: &'a mut B,
-        my_addr: Endpoint,
         paxos: Option<Paxos<'a, C>>,
-        config_id: ConfigId,
     }
     impl<'a, C, B> FastPaxos<'a, C, B>
-    where
-        C: Client,
-        B: Broadcast,
+        where
+            C: Client,
+            B: Broadcast,
     {
         pub async fn propose(&mut self, proposal: Vec<Endpoint>) -> Result<()> {
             let (tx, rx) = oneshot::channel();
@@ -78,9 +64,6 @@ mod consensus {
             self.broadcast.broadcast(request).await;
             rx.await;
             Ok(())
-        }
-        fn get_random_delay(&self) -> Duration {
-            unimplemented!()
         }
     }
 }
